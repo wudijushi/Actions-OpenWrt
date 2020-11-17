0d9d9351b9f15d2f19d4f06a019f5fc648302524
破℃军 <383057973@qq.com>
2020-11-17 10:55:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+cid=$(date "+%Y-%m-%d")
+sed -i "s/R20.10.20/R20.10.20[${cid}]-By JM.liu/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/luci-theme-bootstrap/luci-theme-netgear/g" feeds/luci/collections/luci/Makefile
