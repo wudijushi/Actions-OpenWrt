78ebd3c0fa30393a4a89f7caddde2bcc781ece40
破℃军 <383057973@qq.com>
2021-01-16 11:20:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
-#sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
-
+sed -i '$a src-git kenzok8 https://github.com/wudijushi/openwrt-packages.git' feeds.conf.default
+sed -i '$a src-git small https://github.com/wudijushi/small.git' feeds.conf.default
